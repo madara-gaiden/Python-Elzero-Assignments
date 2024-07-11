7a59e5d474eb8eb11f180474ca4f3ba407b88b84
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-11 02:29:47 +0100

    W2A2

---
+++ W002_Strings_And_Methods/A002.py
@@ -0,0 +1,6 @@
+name = "Madara Gaiden"
+age = "0"
+country = "Morocco"
+
+print("Hello '" + name + "', How You Doing \\\n\"\"\" Your Age Is \"" +
+      age + "\"\" +\n And Your Country Is: " + country)
